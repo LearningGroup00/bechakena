97573cac64609a138fb5a999709253e252fafc01
imfaruk <farukncse@gmail.com>
2023-08-28 12:03:36 +0600

    Add category model

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint
+#
+class Category < ApplicationRecord
+  has_many :categories
+  belongs_to :category, optional: true
+end
