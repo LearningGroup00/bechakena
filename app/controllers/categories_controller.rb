c0533ee26812ed50edb2feab53b205e6df3f647e
imfaruk <farukncse@gmail.com>
2023-09-02 12:48:29 +0600

    Create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @categories = Category.all
   end
@@ -8,9 +10,19 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+    render :new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      # redirect_to category_url(id: category.id)
+      redirect_to category_url(@category)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,4 +33,19 @@ class CategoriesController < ApplicationController
 
   def destroy
   end
+
+  # def category_params
+  #   { name: params[:category][:name], category_id: params[:category][:category_id] }
+  # end
+
+  def category_params
+    params.require(:category).permit(:name, :category_id)
+  end
 end
+
+# params = { name: "value of name" }
+# params[:name]
+
+# params = { category: { name: "user passed value", category_id: 34 }}
+
+# params[:category][:name]
--- app/models/category.rb
+++ app/models/category.rb
@@ -11,4 +11,11 @@
 class Category < ApplicationRecord
   has_many :categories
   belongs_to :category, optional: true
+
+  validates :name, presence: true
+
+
+  def self.parent_categories
+    where(category_id: nil)
+  end
 end
