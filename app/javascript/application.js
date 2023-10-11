3eee80f4bcc28fd7e5be75a43b34ab0db4c5b422
imfaruk <farukncse@gmail.com>
2023-10-11 23:47:54 +0600

    remove everything

--- app/javascript/application.js
+++
@@ -1,5 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import * as bootstrap from "bootstrap"
-import "./controllers"
-
-import { application } from "./application"
