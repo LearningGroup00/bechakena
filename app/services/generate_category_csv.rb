fb5337b68da9f000bce391d229370073aac263e4
imfaruk <farukncse@gmail.com>
2023-11-01 16:49:03 +0600

    add seeds

--- app/services/generate_category_csv.rb
+++ app/services/generate_category_csv.rb
@@ -1,18 +1,18 @@
 require "csv"
 
-class GenerateCategoryCsv 
-  attr_reader :categories 
+class GenerateCategoryCsv
+  attr_reader :categories
 
-  def initialize(categories) 
-    @categories = categories 
-  end 
+  def initialize(categories)
+    @categories = categories
+  end
 
-  def call 
+  def call
     CSV.generate(headers: true) do |csv|
-      #csv << ["ID", "Name", "Details", "Created_at"]
+      csv << ["ID", "Name", "Details", "Created_at"]
       @categories.each do |category|
       csv << [category.id, category.name, category.details, category.created_at ]
-      end 
-    end 
-  end 
-end 
+      end
+    end
+  end
+end
